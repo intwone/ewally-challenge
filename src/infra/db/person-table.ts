3205e268ce4ddf087a575b8c029932da6ef878bf
intwone <cassiointw1993@gmail.com>
2022-12-10 00:43:21 -0300

    feat: add memory db

---
+++ src/infra/db/person-table.ts
@@ -0,0 +1,9 @@
+import { PersonProtocol } from '../../protocols/models/person-model-protocol';
+
+export const personTable = [
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '11111111111',
+    name: 'John Doe',
+  },
+] as PersonProtocol[];
