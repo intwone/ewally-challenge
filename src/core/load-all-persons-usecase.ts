de6da738dce3aa8e9f98b371bb7a5bcc326d7194
intwone <cassiointw1993@gmail.com>
2022-12-10 00:41:38 -0300

    feat: add route to load all persons

--- src/core/load-all-persons-usecase.ts
+++ src/core/load-all-persons-usecase.ts
@@ -1,6 +1,6 @@
-import { LoadAllPersonsUsecaseProtocol } from '../protocols/load-all-persons-usecase-protocol';
 import { PersonProtocol } from '../protocols/models/person-model-protocol';
 import { PersonRepositoryProtocol } from '../protocols/repositories/person-repository-protocol';
+import { LoadAllPersonsUsecaseProtocol } from '../protocols/usecases/load-all-persons-usecase-protocol';
 
 export class LoadAllPersonsUsecase implements LoadAllPersonsUsecaseProtocol {
   constructor(private readonly personRepository: PersonRepositoryProtocol) {}
