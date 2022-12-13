73ad923288779481cbf5a2a1f4ac447d4a7f2445
intwone <cassiointw1993@gmail.com>
2022-12-13 09:31:28 -0300

    chore: add data tests

--- src/infra/db/person-table.ts
+++ src/infra/db/person-table.ts
@@ -1,3 +1,49 @@
 import { PersonProtocol } from '../../protocols/models/person-model-protocol';
 
-export const personTable = [] as PersonProtocol[];
+export const personTable = [
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '11111111111',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '22222222222',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '33333333333',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '44444444444',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '55555555555',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '66666666666',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '77777777777',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '88888888888',
+    name: 'John Doe',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document: '99999999999',
+    name: 'John Doe',
+  },
+] as PersonProtocol[];
--- src/infra/db/relationships-table.ts
+++ src/infra/db/relationships-table.ts
@@ -1,3 +1,44 @@
 import { RelationshipProtocol } from '../../protocols/models/relationship-model-protocol';
 
-export const relationshipsTable = [] as RelationshipProtocol[];
+export const relationshipsTable = [
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d912313939da',
+    document1: '11111111111',
+    document2: '22222222222',
+  },
+  {
+    id: '123de13123b684-520d-43b4-bfd5-1131231231sdad',
+    document2: '33333333333',
+    document1: '11111111111',
+  },
+  {
+    id: '13123fdsf84-520d-43b4-bfd5-c30d912312313939da',
+    document2: '44444444444',
+    document1: '11111111111',
+  },
+  {
+    id: '1231-520d-43b4-bfd5-3123',
+    document1: '22222222222',
+    document2: '33333333333',
+  },
+  {
+    id: 'fdvs131-520d-43b4-bfd5-123',
+    document1: '22222222222',
+    document2: '88888888888',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d9112313313939da',
+    document1: '88888888888',
+    document2: '55555555555',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d9112313313939da',
+    document1: '88888888888',
+    document2: '99999999999',
+  },
+  {
+    id: '123deb684-520d-43b4-bfd5-c30d9112313313939da',
+    document1: '22222222222',
+    document2: '99999999999',
+  },
+] as RelationshipProtocol[];
